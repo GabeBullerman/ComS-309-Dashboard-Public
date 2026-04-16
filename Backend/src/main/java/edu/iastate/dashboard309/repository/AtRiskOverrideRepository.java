fdceffa2747206d7ed5f764eee67592ec6e4801f
Gabe <gbulle@iastate.edu>
2026-04-16 16:32:23 -0500

    Fixed removing of manual flag "at-risk" on student

--- Backend/src/main/java/edu/iastate/dashboard309/repository/AtRiskOverrideRepository.java
+++ Backend/src/main/java/edu/iastate/dashboard309/repository/AtRiskOverrideRepository.java
@@ -2,11 +2,13 @@ package edu.iastate.dashboard309.repository;
 
 import edu.iastate.dashboard309.model.AtRiskOverride;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface AtRiskOverrideRepository extends JpaRepository<AtRiskOverride, Long> {
     List<AtRiskOverride> findByStudentNetid(String studentNetid);
     boolean existsByStudentNetid(String studentNetid);
+    @Transactional
     void deleteByStudentNetid(String studentNetid);
 }
